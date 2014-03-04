338c454869e15f2619c537d11d8a722aaeab9001
Piotr Kosiakowski <conan23@gmail.com>
2014-03-04 16:52:02 +0100

    Config change

--- config.php
+++ config.php
@@ -1,7 +1,5 @@
 <?php
 
-die('----: ' . $_SERVER['RDS_USERNAME']);
-
 require_once('config/config_prod.php');
 
 
---
+++ config/config_prod.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$dbuser = $_SERVER['RDS_USERNAME'];
+$dbpass = $_SERVER['RDS_PASSWORD'];
+$dbname = $_SERVER['RDS_DB_NAME'];
+
+$accessPWD = $_SERVER['RDS_PASSWORD'];
